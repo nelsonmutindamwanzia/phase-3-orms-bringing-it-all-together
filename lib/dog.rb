c4c145c7eeeb4677b97d4be917b86d7fd316a9a3
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-10-31 16:58:31 +0300

    added methods except .save method

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,78 @@
 class Dog
+    @@all = []
+
+    attr_accessor :name, :breed, :id
+
+    def initialize(name:, breed:, id: nil)
+        @id = id
+        @name = name
+        @breed = breed
+        @@all << self
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+            SQL
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = <<-SQL
+            DROP TABLE dogs
+            SQL
+            DB[:conn].execute(sql)
+    end
+
+    # def save(database_connection)
+    #     database_connection.execute()
+    # end
+
+    def self.create(name:, breed:)
+        dog = Dog.new(name: name, breed: breed)
+        dog.save
+    end
+
+    def self.new_from_db(row)
+        self.new(id: row[0], name: row[1], breed: row[2])
+    end
+
+    def self.all
+        sql = <<-SQL
+            SELECT * 
+            FROM dogs;
+            SQL
+        DB[:conn].execute(sql).map do |row|
+            self.new_from_db(row)
+        end
+    end
+
+    def self.find_by_name(name)
+        sql = <<-SQL
+            SELECT * 
+            FROM dogs
+            WHERE name = ?
+            LIMIT 1;
+            SQL
+        DB[:conn].execute(sql, name).map do |row|
+            self.new_from_db(row)
+        end.first
+    end
+
+    def self.find(id)
+        sql = <<-SQL
+            SELECT *
+            FROM dogs
+            WHERE dogs.id = ?
+            LIMIT 1;
+            SQL
+        DB[:conn].execute(sql, id).map do |row|
+            self.new_from_db(row)
+        end.first
+    end
 
 end
